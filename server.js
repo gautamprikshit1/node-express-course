bc84fe49671cfb2a0450d8038e5b2981d788e855
priks <priks@pop-os.localdomain>
2020-12-06 23:39:00 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Prikshit'},
+    {name: 'Gautam'}
+];
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: "Successfully got users",
+        users: mockUserData
+    });
+});
+
 app.listen(8000, () => {console.log("Hello World")});
