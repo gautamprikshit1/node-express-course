4b877a0f88d806c4fb6bf9b36854dcb4e998ba90
priks <priks@pop-os.localdomain>
2020-12-07 11:15:14 +0530

    add POST route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const mockUserData = [
     {name: 'Prikshit'},
@@ -13,6 +14,26 @@ app.get('/users/:id', (req, res) => {
         message: "Got one user",
         users: req.params.id
     });
+});
+
+app.post('/login', (req, res) => {
+    const username = body.req.username;
+    const password = body.req.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "password";
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'Login successful',
+            token: 'Encryption token goes here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: "Please enter coorect username and password"
+        });
+    }
 });
 
 app.listen(8000, () => {console.log("Hello World")});
